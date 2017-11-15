9517b8fa9f21ec4ac667ea7889551abcfb80d78a
Samvid Pandya <samvid.pandya150@gmail.com>
2017-11-15 11:34:02 -0800

    Delete Car.py

--- Lesson/myPackage/Car.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Bryan Cairns'
-
-class Car(object):
-    def setSpeed(self, speed):
-        print("Going this fast: %d" % speed)
-
-class Truck(object):
-    def setSpeed(self, speed):
-        print("Going this fast: %d" % speed)
