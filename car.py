5a68fd0c79cfb0332e7bd72c73688a67ad9fe383
Samvid Pandya <samvid.pandya150@gmail.com>
2017-11-15 11:32:50 -0800

    Car Python

---
+++ car.py
@@ -0,0 +1,7 @@
+class Car(object):
+    def setSpeed(self, speed):
+        print("Going this fast: %d" % speed)
+
+class Truck(object):
+    def setSpeed(self, speed):
+        print("Going this fast: %d" % speed)
